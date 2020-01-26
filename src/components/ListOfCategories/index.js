3174827846fc78e2eda707eb85b4bc5927d83f63
Veronica Paola Medina Cornejo <vmedina@inlearning.edu.pe>
2020-01-26 10:59:25 -0500

    cargar petgram

---
+++ src/components/ListOfCategories/index.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from 'react'
+import { Category } from '../Category'
+
+import { List, Item } from './styles'
+import { categories as mockCategories } from '../../../api/db.json'
+
+export const ListOfCategories = () => {
+  const [categories, setCategories] = useState(mockCategories)
+
+  return (
+    <List>
+      {
+        categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
+      }
+    </List>
+  )
+}
---
+++ src/components/ListOfCategories/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const List = styled.ul`
+  display: flex;
+  overflow: hidden;
+  width: 100%;
+`
+
+export const Item = styled.li`
+  padding: 0 8px;
+`
